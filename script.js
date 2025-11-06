fb25f19271ce33e4ebe5753bc15cbed0374ca8a1
StaleGradov <semargl877@mail.ru>
2025-11-06 12:57:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ class HeroGame {
         this.maps = [];          // Список всех карт
         this.locations = [];     // Список всех локаций
         
+        // Система бонусов
+        this.bonusSystem = new BonusSystem();
+        
         // Флаги отображения
         this.showReward = false;         // Показывать ли награду
         this.lastReward = 0;             // Последняя полученная награда
@@ -63,9 +66,13 @@ class HeroGame {
 
     // ========== МОДУЛЬ 1.2: ОСНОВНОЙ МЕТОД ИНИЦИАЛИЗАЦИИ ==========
     async init() {
-        await this.loadGameData();    // Загрузка всех данных игры
-        this.initLocationSystem();    // Инициализация системы локаций
-        this.loadSave();              // Загрузка сохраненной игры
+        // Сначала инициализируем систему бонусов
+        await this.bonusSystem.loadBonusData();
+        
+        // Затем загружаем остальные данные игры
+        await this.loadGameData();    
+        this.initLocationSystem();    
+        this.loadSave();              
         
         // Разблокировка первого героя по умолчанию
         if (this.heroes.length > 0) {
@@ -75,7 +82,7 @@ class HeroGame {
             }
         }
         
-        this.renderHeroSelect();      // Показ экрана выбора героя
+        this.renderHeroSelect();      
     }
 
     // ========== МОДУЛЬ 1.3: ИНИЦИАЛИЗАЦИЯ СИСТЕМЫ ЛОКАЦИЙ ==========
@@ -101,7 +108,6 @@ class HeroGame {
         });
     }
 }
-
 // ========== МОДУЛЬ 2: ИНИЦИАЛИЗАЦИЯ И ЗАГРУЗКА ДАННЫХ ==========
 
 // ========== МОДУЛЬ 2.1: ЗАГРУЗКА JSON ФАЙЛА ==========
@@ -257,265 +263,12 @@ HeroGame.prototype.createFallbackData = function() {
 
 // ========== МОДУЛЬ 3.1: ПОЛУЧЕНИЕ ВСЕХ ДОСТУПНЫХ БОНУСОВ ==========
 HeroGame.prototype.getBonuses = function() {
-    return {
-        races: {
-            elf: { type: "damage_mult", value: 0.2, name: "Эльф", description: "Урон +20%", source: "race" },
-            halfling: { type: "crit_chance", value: 0.2, name: "Полурослик", description: "20% шанс двойного урона", source: "race" },
-            human: { type: "gold_mult", value: 0.3, name: "Человек", description: "+30% золота за противника", source: "race" },
-            laitar: { type: "vampirism", value: 0.05, name: "Лайтар", description: "5% урона восстанавливает здоровье", source: "race" },
-            ork: { type: "health_regen_mult", value: 0.3, name: "Орк", description: "+30% к регенерации здоровья", source: "race" },
-            dwarf: { type: "health_mult", value: 0.3, name: "Гном", description: "+30% к здоровью", source: "race" },
-            dragon: { type: "armor_mult", value: 0.15, name: "Дракон", description: "+15% к броне", source: "race" },
-            fairy: { type: "armor_penetration", value: 0.25, name: "Фея", description: "25% шанс игнорировать броню", source: "race" }
-        },
-        classes: {
-            hunter: { type: "armor_penetration", value: 0.25, name: "Охотник", description: "25% шанс игнорировать броню", source: "class" },
-            warrior: { type: "armor_mult", value: 0.15, name: "Воин", description: "+15% к броне", source: "class" },
-            bounty_hunter: { type: "crit_chance", value: 0.2, name: "Охотник за головами", description: "20% шанс двойного урона", source: "class" },
-            merchant: { type: "gold_mult", value: 0.3, name: "Торговец", description: "+30% золота за противника", source: "class" },
-            thief: { type: "gold_mult", value: 0.3, name: "Вор", description: "+30% золота за противника", source: "class" },
-            fighter: { type: "health_regen_mult", value: 0.3, name: "Кулачный боец", description: "+30% к регенерации", source: "class" },
-            antiquarian: { type: "gold_mult", value: 0.3, name: "Искатель древностей", description: "+30% золота за противника", source: "class" },
-            death_mage: { type: "vampirism", value: 0.05, name: "Волхв смерти", description: "5% урона восстанавливает здоровье", source: "class" },
-            sorcerer: { type: "damage_mult", value: 0.2, name: "Колдун", description: "Урон +20%", source: "class" },
-            archer: { type: "crit_chance", value: 0.2, name: "Лучник", description: "20% шанс двойного урона", source: "class" },
-            healer: { type: "health_mult", value: 0.3, name: "Знахарь", description: "+30% к здоровью", source: "class" },
-            gladiator: { type: "damage_mult", value: 0.2, name: "Гладиатор", description: "Урон +20%", source: "class" },
-            blacksmith: { type: "armor_mult", value: 0.15, name: "Кузнец", description: "+15% к броне", source: "class" }
-        },
-        sagas: {
-            golden_egg: { type: "health_mult", value: 0.3, name: "Золотое Яйцо", description: "+30% к здоровью", source: "saga" },
-            vulkanor: { type: "armor_penetration", value: 0.25, name: "Вулканор", description: "25% шанс игнорировать броню", source: "saga" },
-            well: { type: "gold_mult", value: 0.3, name: "Колодец", description: "+30% золота за противника", source: "saga" },
-            pets: { type: "damage_mult", value: 0.2, name: "Питомец", description: "Урон +20%", source: "saga" },
-            following_sun: { type: "health_regen_mult", value: 0.3, name: "Вслед за солнцем", description: "+30% к регенерации", source: "saga" },
-            vampire_crown: { type: "vampirism", value: 0.05, name: "Корона короля вампиров", description: "5% урона восстанавливает здоровье", source: "saga" },
-            tiger_eye: { type: "crit_chance", value: 0.2, name: "Желтый Глаз тигра", description: "20% шанс двойного урона", source: "saga" },
-            sky_phenomena: { type: "armor_mult", value: 0.15, name: "Небесные явления", description: "+15% к броне", source: "saga" }
-        }
-    };
+    return this.bonusSystem.getBonuses();
 };
 
 // ========== МОДУЛЬ 3.2: КОНФИГУРАЦИЯ СЕТОВ ПРЕДМЕТОВ ==========
 HeroGame.prototype.getItemSetConfig = function() {
-    return {
-        "set_beginner": {
-            name: "Крестьянина Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "damage_mult", value: 0.05 },
-            description: "Комплект из 6 вещей даст +5% к урону"
-        },
-        "set_warrior": {
-            name: "Ополченца Арканиума", 
-            requiredPieces: 6,
-            bonus: { type: "damage_mult", value: 0.1 },
-            description: "Комплект из 6 вещей даст +10% к урону"
-        },
-        "set_guardian": {
-            name: "Милитанта Арканиума",
-            requiredPieces: 6, 
-            bonus: { type: "damage_mult", value: 0.15 },
-            description: "Комплект из 6 вещей даст +15% к урону"
-        },
-        "set_hunter": {
-            name: "Ветерана Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "damage_mult", value: 0.2 },
-            description: "Комплект из 6 вещей даст +20% к урону"
-        },
-        "set_complete": {
-            name: "Командира ополчения Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "damage_mult", value: 0.25 },
-            description: "Комплект из 6 вещей даст +25% к урону"
-        },
-          "set_king": {
-            name: "Стратегоса Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "damage_mult", value: 0.3 },
-            description: "Комплект из 6 вещей даст +30% к урону"
-        },
-         "set_crit1": {
-            name: "Охотника Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "crit_chance", value: 0.05 },
-            description: "Комплект из 6 вещей даст +5% к шансу критического удара(наносящего х2 урона)"
-        },
-     "set_crit2": {
-            name: "Разведчика Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "damage_mult", value: 0.1 },
-            description: "Комплект из 6 вещей даст +10% к шансу критического удара(наносящего х2 урона)"
-        },
-     "set_crit3": {
-            name: "Лучника Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "damage_mult", value: 0.15 },
-            description: "Комплект из 6 вещей даст +15% к шансу критического удара(наносящего х2 урона)"
-        },
-     "set_crit4": {
-            name: "Элитного стрелка Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "damage_mult", value: 0.2 },
-            description: "Комплект из 6 вещей даст +20% к шансу критического удара(наносящего х2 урона)"
-        },
-     "set_crit5": {
-            name: "Командира лучников Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "damage_mult", value: 0.25 },
-            description: "Комплект из 6 вещей даст +25% к шансу критического удара(наносящего х2 урона)"
-        },
-     "set_crit6": {
-            name: "Легендарного стрелка Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "damage_mult", value: 0.3 },
-            description: "Комплект из 6 вещей даст +30% к шансу критического удара(наносящего х2 урона)"
-        },
-             "set_penetration1": {
-            name: "Стрелка Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "armor_penetration", value: 0.06 },
-            description: "Комплект из 6 вещей даст +6% к шансу игрорирования брони соперника"
-        },
-         "set_penetration2": {
-            name: "Следопыта Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "armor_penetration", value: 0.12 },
-            description: "Комплект из 6 вещей даст +12% к шансу игрорирования брони соперника"
-        },
-         "set_penetration3": {
-            name: "Охотника на монстров Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "armor_penetration", value: 0.18 },
-            description: "Комплект из 6 вещей даст +18% к шансу игрорирования брони соперника"
-        },
-         "set_penetration4": {
-            name: "Наемного убийцы Магнатов Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "armor_penetration", value: 0.24 },
-            description: "Комплект из 6 вещей даст +24% к шансу игрорирования брони соперника"
-        },
-         "set_penetration5": {
-            name: "Командира арбалетчиков Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "armor_penetration", value: 0.3 },
-            description: "Комплект из 6 вещей даст +30% к шансу игрорирования брони соперника"
-        },
-         "set_penetration6": {
-            name: "Мастера над арбалетами Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "armor_penetration", value: 0.36 },
-            description: "Комплект из 6 вещей даст +36% к шансу игрорирования брони соперника"
-        },
-           "set_rich1": {
-            name: "Сборщика трофеев Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "gold_mult", value: 0.05 },
-            description: "Комплект из 6 вещей даст +5% к награде в золоте за убийство монстра"
-        },
-           "set_rich2": {
-            name: "Охотник на редких животных Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "gold_mult", value: 0.1 },
-            description: "Комплект из 6 вещей даст +10% к награде в золоте за убийство монстра"
-        },
-           "set_rich3": {
-            name: "Профессионального истребителя опасных существ Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "gold_mult", value: 0.15 },
-            description: "Комплект из 6 вещей даст +15% к награде в золоте за убийство монстра"
-        },
-           "set_rich4": {
-            name: "Коллекционера Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "gold_mult", value: 0.2 },
-            description: "Комплект из 6 вещей даст +20% к награде в золоте за убийство монстра"
-        },
-           "set_rich5": {
-            name: "Ловца королевских тварей Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "gold_mult", value: 0.25 },
-            description: "Комплект из 6 вещей даст +25% к награде в золоте за убийство монстра"
-        },
-           "set_rich6": {
-            name: "Легендарного зверолова Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "gold_mult", value: 0.3 },
-            description: "Комплект из 6 вещей даст +30% к награде в золоте за убийство монстра"
-        },
-            "set_vampire1": {
-            name: "Убийцы Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "vampirism", value: 0.01 },
-            description: "Комплект из 6 вещей даст +1% к вампиризму"
-        },
-           "set_vampire2": {
-            name: "Наемного убийцы Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "vampirism", value: 0.02 },
-            description: "Комплект из 6 вещей даст +2% к вампиризму"
-        },
-            "set_vampire3": {
-            name: "Темного стража Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "vampirism", value: 0.03 },
-            description: "Комплект из 6 вещей даст +3% к вампиризму"
-        },
-               "set_vampire4": {
-            name: "Легендарного зверолова Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "vampirism", value: 0.04 },
-            description: "Комплект из 6 вещей даст +4% к вампиризму"
-        },
-               "set_vampire5": {
-            name: "Охотника на вампиров, ставшего вампиром",
-            requiredPieces: 6,
-            bonus: { type: "vampirism", value: 0.05 },
-            description: "Комплект из 6 вещей даст +5% к вампиризму"
-        },
-               "set_vampire6": {
-            name: "Лорда вампиров...Арканиума? Откуда у лорда вампиров могли взяться доспехи из костей драконов..? Неужели драконы были здесь во времена вампиров?  ",
-            requiredPieces: 6,
-            bonus: { type: "vampirism", value: 0.06 },
-            description: "Комплект из 6 вещей даст +6% к вампиризму"
-        },
-            "set_regen1": {
-            name: "Грабителя Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "health_regen_mult", value: 0.5 },
-            description: "Комплект из 6 вещей даст +5% к регенерации здоровья"
-        },
-            "set_regen2": {
-            name: "Бандита Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "health_regen_mult", value: 0.1 },
-            description: "Комплект из 6 вещей даст +10% к регенерации здоровья"
-        },
-            "set_regen3": {
-            name: "Опытного разбойника",
-            requiredPieces: 6,
-            bonus: { type: "health_regen_mult", value: 0.2 },
-            description: "Комплект из 6 вещей даст +20% к регенерации здоровья"
-        },
-                   "set_regen4": {
-            name: "Вожака банды Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "health_regen_mult", value: 0.4 },
-            description: "Комплект из 6 вещей даст +40% к регенерации здоровьяу"
-        },
-                   "set_regen5": {
-            name: "Берсерка, лучшего бойца воровской гильдии Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "health_regen_mult", value: 0.8 },
-            description: "Комплект из 6 вещей даст +80% к регенерации здоровья"
-        },
-                   "set_regen6": {
-            name: "Короля воров Арканиума",
-            requiredPieces: 6,
-            bonus: { type: "health_regen_mult", value: 1.6 },
-            description: "Комплект из 6 вещей даст +160% к регенерации здоровья"
-        }
-    };
+    return this.bonusSystem.getItemSetConfig();
 };
 
 // ========== МОДУЛЬ 3.3: ПОЛУЧЕНИЕ АКТИВНЫХ СЕТОВ ==========
@@ -537,16 +290,17 @@ HeroGame.prototype.getActiveSetBonuses = function(hero) {
     });
     
     const activeSetBonuses = [];
+    const setConfig = this.bonusSystem.getItemSetConfig();
     
     // Проверяем условия для каждого сета
     Object.keys(setCounts).forEach(setName => {
-        const setConfig = this.getItemSetConfig()[setName];
-        if (setConfig && setCounts[setName] >= setConfig.requiredPieces) {
+        const setConfigItem = setConfig[setName];
+        if (setConfigItem && setCounts[setName] >= setConfigItem.requiredPieces) {
             activeSetBonuses.push({
                 setName: setName,
                 pieces: setCounts[setName],
-                bonus: setConfig.bonus,
-                description: setConfig.description
+                bonus: setConfigItem.bonus,
+                description: setConfigItem.description
             });
         }
     });
@@ -559,36 +313,16 @@ HeroGame.prototype.getAllActiveBonuses = function(hero) {
     hero = hero || this.currentHero;
     if (!hero) return { race: [], class: [], saga: [], equipment: [], sets: [] };
     
-    const bonuses = this.getBonuses();
-    const activeBonuses = {
-        race: [],
-        class: [],
-        saga: [], 
-        equipment: [],
-        sets: [] // НОВОЕ: бонусы от сетов
-    };
+    // Получаем базовые бонусы из системы
+    const baseBonuses = this.bonusSystem.getAllActiveBonuses(hero);
     
-    // Бонусы от расы
-    if (bonuses.races[hero.race]) {
-        activeBonuses.race.push(bonuses.races[hero.race]);
-    }
-    
-    // Бонусы от класса
-    if (bonuses.classes[hero.class]) {
-        activeBonuses.class.push(bonuses.classes[hero.class]);
-    }
-    
-    // Бонусы от саги
-    if (bonuses.sagas[hero.saga]) {
-        activeBonuses.saga.push(bonuses.sagas[hero.saga]);
-    }
-    
-    // Бонусы от экипировки
+    // Добавляем бонусы от экипировки
+    const equipmentBonuses = [];
     Object.values(hero.equipment).forEach(itemId => {
         if (itemId) {
             const item = this.items.find(item => item.id === itemId);
             if (item && item.bonus) {
-                activeBonuses.equipment.push({
+                equipmentBonuses.push({
                     ...item.bonus,
                     source: "equipment",
                     itemName: item.name
@@ -597,19 +331,23 @@ HeroGame.prototype.getAllActiveBonuses = function(hero) {
         }
     });
     
-    // НОВОЕ: Бонусы от сетов
+    // Добавляем бонусы от сетов
     const setBonuses = this.getActiveSetBonuses(hero);
-    setBonuses.forEach(setBonus => {
-        activeBonuses.sets.push({
-            ...setBonus.bonus,
-            source: "set",
-            setName: setBonus.setName,
-            pieces: setBonus.pieces,
-            description: setBonus.description
-        });
-    });
+    const setBonusObjects = setBonuses.map(setBonus => ({
+        ...setBonus.bonus,
+        source: "set",
+        setName: setBonus.setName,
+        pieces: setBonus.pieces,
+        description: setBonus.description
+    }));
     
-    return activeBonuses;
+    return {
+        race: baseBonuses.race,
+        class: baseBonuses.class,
+        saga: baseBonuses.saga,
+        equipment: equipmentBonuses,
+        sets: setBonusObjects
+    };
 };
 
 // ========== МОДУЛЬ 3.5: РАСЧЕТ СУММАРНЫХ БОНУСОВ ==========
@@ -1247,19 +985,8 @@ HeroGame.prototype.renderHeroSelect = function() {
 
 // ========== МОДУЛЬ 9.2: ПОЛУЧЕНИЕ ИКОНКИ ДЛЯ БОНУСА ==========
 HeroGame.prototype.getBonusIcon = function(bonusType) {
-    const icons = {
-        'health_mult': '❤️',
-        'damage_mult': '⚔️',
-        'armor_mult': '🛡️',
-        'gold_mult': '💰',
-        'health_regen_mult': '⚡',
-        'crit_chance': '💥',
-        'armor_penetration': '⚡',
-        'vampirism': '🩸'
-    };
-    return icons[bonusType] || '🎯';
+    return this.bonusSystem.getBonusIcon(bonusType);
 };
-
 // ========== МОДУЛЬ 9.3: ВЫБОР ГЕРОЯ ==========
 HeroGame.prototype.selectHero = function(heroId) {
     const hero = this.heroes.find(h => h.id === heroId);
