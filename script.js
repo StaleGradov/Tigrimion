f8c8095dcb64cc0aadd8db13a88ed75d52c2c881
StaleGradov <semargl877@mail.ru>
2025-11-19 22:39:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -746,8 +746,6 @@ class SafeHeroGame {
                 <div class="top-action-bar">
                     <button class="btn-top" onclick="game.showOverlay('global-map')">🗺️ Глобальная карта</button>
                     <button class="btn-top" onclick="game.showOverlay('local-map')">📍 Локальная карта</button>
-                    <button class="btn-top" onclick="game.showOverlay('tactical-map')">🎲 Тактическая карта</button>
-                    <button class="btn-top" onclick="game.systems.map.showTacticalMapEditor()">🎨 Создать карту</button>
                     <button class="btn-top" onclick="game.showOverlay('inventory')">🎒 Инвентарь</button>
                     <button class="btn-top" onclick="game.showOverlay('shop')">🏪 Магазин</button>
                     <button class="btn-top" onclick="game.showHeroSelection()">🔁 Сменить героя</button>
